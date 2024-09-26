4c42029a1a2423a0a55524f115d372b7c2e922f6
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-09-26 18:38:02 -0300

    add shadcn ui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main className="h-screen w-full flex items-center justify-center">
-      <h1 className="text-3xl font-bold">🧠 Quizmoji 🏆</h1>
+      <div className="flex flex-col gap-8">
+        <h1 className="text-3xl font-bold">🧠 Quizmoji 🏆</h1>
+        <Button>Iniciar Quiz</Button>
+      </div>
     </main>
   )
 }
