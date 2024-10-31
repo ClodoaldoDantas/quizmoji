423401f7159aa0a3e211f20355be0e5357dda3b7
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-10-31 18:58:45 -0300

    Update grid layout in HomeLoading component

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export default function HomeLoading() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="max-w-4xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-6">
       <Skeleton className="h-[150px]" />
       <Skeleton className="h-[150px]" />
       <Skeleton className="h-[150px]" />
